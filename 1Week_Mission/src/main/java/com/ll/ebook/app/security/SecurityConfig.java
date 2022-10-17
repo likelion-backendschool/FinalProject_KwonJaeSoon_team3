13897ab6ad98a3e0c75f97bd0ebf57cb7a303e33
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-10-17 17:44:56 +0900

    Feat : Logout

--- 1Week_Mission/src/main/java/com/ll/ebook/app/security/SecurityConfig.java
+++ 1Week_Mission/src/main/java/com/ll/ebook/app/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -26,6 +27,12 @@ public class SecurityConfig {
                         formLogin -> formLogin
                                 .loginPage("/member/login") // GET
                                 .loginProcessingUrl("/member/login") // POST
+                )
+                .logout(
+                        logout -> logout
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+                                .logoutSuccessUrl("/?msg=Logout!!! ")
+                                .invalidateHttpSession(true)
                 );
 
         return http.build();
