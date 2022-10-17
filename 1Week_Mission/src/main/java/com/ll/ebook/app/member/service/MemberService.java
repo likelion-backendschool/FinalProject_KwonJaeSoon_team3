54fd892648630eb5ffecb94e0dee28d9ba3f73a1
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-10-17 16:36:45 +0900

    Feat : UserModify

--- 1Week_Mission/src/main/java/com/ll/ebook/app/member/service/MemberService.java
+++ 1Week_Mission/src/main/java/com/ll/ebook/app/member/service/MemberService.java
@@ -19,7 +19,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class MemberService implements UserDetailsService {
+public class MemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
@@ -43,17 +43,10 @@ public class MemberService implements UserDetailsService {
         return memberRepository.findByUsername(username).orElse(null);
     }
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Member member = memberRepository.findByUsername(username).get();
+    public void modify(Member member, String nickname, String email) {
+        member.setNickname(nickname);
+        member.setEmail(email);
 
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        if (member.getNickname() != null) {
-            authorities.add(new SimpleGrantedAuthority("writer"));
-        } else {
-            authorities.add(new SimpleGrantedAuthority("member"));
-        }
-
-        return new User(member.getUsername(), member.getPassword(), authorities);
+        memberRepository.save(member);
     }
 }
