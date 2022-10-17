933a3e7c82c7a029eb3810fdbae9e345f6bb63bd
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-10-17 13:34:58 +0900

    Feat : Join

---
+++ 1Week_Mission/src/main/java/com/ll/ebook/app/member/form/JoinForm.java
@@ -0,0 +1,15 @@
+package com.ll.ebook.app.member.form;
+
+import lombok.Data;
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class JoinForm {
+    @NotEmpty
+    private String username;
+    @NotEmpty
+    private String password;
+    private String nickname;
+    @NotEmpty
+    private String email;
+}
