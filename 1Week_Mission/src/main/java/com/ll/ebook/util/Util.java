943987b38ef4b467ae37440ddaf88aab1d2f97aa
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-10-22 23:01:10 +0900

    Refactor : Handler 추가

--- 1Week_Mission/src/main/java/com/ll/ebook/util/Util.java
+++ 1Week_Mission/src/main/java/com/ll/ebook/util/Util.java
@@ -5,6 +5,42 @@ import java.net.URLEncoder;
 
 public class Util {
     public static class url {
+        public static boolean isUrl(String url) {
+            if (url == null) return false;
+            return url.matches("^(https?):\\/\\/([^:\\/\\s]+)(:([^\\/]*))?((\\/[^\\s/\\/]+)*)?\\/?([^#\\s\\?]*)(\\?([^#\\s]*))?(#(\\w*))?$");
+        }
+
+        public static String addQueryParam(String url, String paramName, String paramValue) {
+            if (url.contains("?") == false) {
+                url += "?";
+            }
+
+            url += paramName + "=" + encode(paramValue);
+
+            return url;
+        }
+
+        public static String modifyQueryParam(String url, String paramName, String paramValue) {
+            url = deleteQueryParam(url, paramName);
+            url = addQueryParam(url, paramName, paramValue);
+
+            return url;
+        }
+
+        private static String deleteQueryParam(String url, String paramName) {
+            int startPoint = url.indexOf(paramName + "=");
+            if (startPoint == -1) return url;
+
+            int endPoint = url.substring(startPoint).indexOf("&");
+
+            if (endPoint == -1) {
+                return url.substring(0, startPoint - 1);
+            }
+
+            String urlAfter = url.substring(startPoint + endPoint + 1);
+
+            return url.substring(0, startPoint) + urlAfter;
+        }
         public static String encode(String str) {
             try {
                 return URLEncoder.encode(str, "UTF-8");
